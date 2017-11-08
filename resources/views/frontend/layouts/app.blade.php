9d7f86bd201816503c5653c5e4b4874f01161971
hoaunet <hoaunet@yahoo.com>
2017-11-08 16:49:43 +0700

    update

---
+++ resources/views/frontend/layouts/app.blade.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="ja"><!-- InstanceBegin template="/Templates/second.dwt" codeOutsideHTMLIsLocked="false" --><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="description" content="薬学部、危機管理学部、看護学部からなる千葉科学大学は、千葉県銚子市に誕生した大学です。">
+<meta name="keywords" content="薬学部,危機管理,看護,教員採用試験,公務員試験,獣医,千葉科学大学,大学受験,大学入試,大学案内,倍率">
+<meta name="robots" content="INDEX,FOLLOW">
+<meta http-equiv="imagetoolbar" content="no">
+<!-- InstanceBeginEditable name="doctitle" -->
+<title>千葉科学大学 薬学部・危機管理学部・看護学部</title>
+<link rel="stylesheet" href="http://www.cis.ac.jp/layout.css" type="text/css">
+<!-- InstanceEndEditable -->
+<link rel="shortcut icon" href="http://www.cis.ac.jp/{{ asset('') }}public/common/img/favicon.ico">
+<link rel="stylesheet" href="http://www.cis.ac.jp/common/css/base.css" type="text/css" media="screen,print">
+<link rel="alternate" media="handheld" type="text/html" href="http://www.sp.cis.ac.jp/">
+<script src="http://www.cis.ac.jp/common/js/jquery-1.7.2.min.js"></script>
+<script src="http://www.cis.ac.jp/common/js/common.js"></script>
+<!--[if lt IE 9]> 
+<script src="http://www.cis.ac.jp/common/js/html5shiv.js"></script>
+<![endif]-->
+<!-- InstanceBeginEditable name="pagescript" -->
+<script type="text/javascript" src="http://www.cis.ac.jp/top.js"></script>
+<!-- InstanceEndEditable -->
+</head>
+<body>
+<div id="wrapper"> 
+  <!-- #header -->
+  <header id="global-header">
+    <div class="header-contents">
+      <ul id="tnav">
+        <li id="tnav01"><a href="../index.html"><img src="{{ asset('') }}public/common/img/tnav_home_out.gif" alt="Home" width="80" height="70"></a></li>
+        <li id="tnav02"><a href="../examinee/index.html"><img src="{{ asset('') }}public/common/img/tnav_examinee_out.gif" alt="受験生の方へ" width="180" height="70"></a></li>
+        <li id="tnav03"><a href="index.html"><img src="{{ asset('') }}public/common/img/tnav_student_out.gif" alt="在学生の方へ" width="180" height="70"></a></li>
+        <li id="tnav04"><a href="../graduate/index.html"><img src="{{ asset('') }}public/common/img/tnav_graduate_out.gif" alt="卒業生の方へ" width="180" height="70"></a></li>
+        <li id="tnav05"><a href="../guardian/index.html"><img src="{{ asset('') }}public/common/img/tnav_guardian_out.gif" alt="保護者の方へ" width="180" height="70"></a></li>
+        <li id="tnav06"><a href="../research/index.html"><img src="{{ asset('') }}public/common/img/tnav_educational_out.gif" alt="一般・教育機関の方へ" width="200" height="70"></a></li>
+      </ul>
+      <h1 id="logo"><a href="http://www.cis.ac.jp/"><img src="{{ asset('') }}public/common/img/logo.gif" alt="千葉科学大学" width="312" height="55"></a></h1>
+      <ul id="snav">
+        <li><a href="../mobile/index.html"><img src="{{ asset('') }}public/common/img/subnav_mobile_off.gif" alt="モバイルサイト" width="100" height="18"></a></li>
+        <li><a href="../information/access/index.html"><img src="{{ asset('') }}public/common/img/subnav_access_off.gif" alt="アクセス" width="70" height="18"></a></li>
+        <li><a href="../contact/index.html"><img src="{{ asset('') }}public/common/img/subnav_contact_off.gif" alt="お問い合わせ" width="101" height="18"></a></li>
+        <li><a href="../shiryo/index.html"><img src="{{ asset('') }}public/common/img/subnav_shiryo_off.gif" alt="資料請求" width="75" height="18"></a></li>
+      </ul>
+      <div id="language_btn"><a href="../en/index.html">English</a></div>
+      <div id="search">
+        <form action="http://www.cis.ac.jp/search/" id="cse-search-box">
+          <input type="hidden" name="cx" value="014121292759862374546:hxbzafa7pby">
+          <input type="hidden" name="cof" value="FORID:11">
+          <input type="hidden" name="ie" value="UTF-8">
+          <input type="text" name="q" value="サイト内検索" id="field">
+          <input type="image" name="sa" src="{{ asset('') }}public/common/img/ico_sitesearch.gif" alt="サイト内検索" id="search-btn">
+        </form>
+        <script type="text/javascript" src="http://www.google.com/jsapi"></script> 
+        <script type="text/javascript">google.load("elements", "1", {packages: "transliteration"});</script> 
+      </div>
+      <ul id="gnav">
+        <li id="gnav01"><a href="../opencampus/index.html"><img src="{{ asset('') }}public/common/img/gnav_opencampus_off.gif" alt="オープンキャンパス" width="190" height="66"></a></li>
+        <li id="gnav02"><a href="../information/index.html"><img src="{{ asset('') }}public/common/img/gnav_info_off.gif" alt="大学案内" width="140" height="66"></a></li>
+        <li id="gnav03"><a href="../information/introduction/pharmacy/index.html"><img src="{{ asset('') }}public/common/img/gnav_pharmacy_off.gif" alt="薬学部" width="120" height="66"></a></li>
+        <li id="gnav04"><a href="../information/introduction/crisis/index.html"><img src="{{ asset('') }}public/common/img/gnav_crisis_off.gif" alt="危機管理学部" width="150" height="66"></a></li>
+        <li id="gnav05"><a href="../information/introduction/nursing/index.html"><img src="{{ asset('') }}public/common/img/gnav_nursing_off.gif" alt="看護学部" width="140" height="66"></a></li>
+        <li id="gnav06"><a href="../information/introduction/index.html#grad"><img src="{{ asset('') }}public/common/img/gnav_grad_off.gif" alt="大学院" width="120" height="66"></a></li>
+        <li id="gnav07"><a href="../information/introduction/bekka/index.html"><img src="{{ asset('') }}public/common/img/gnav_bekka_off.gif" alt="留学生別科" width="140" height="66"></a></li>
+      </ul>
+    </div>
+  </header>
+  <!-- /#globa-header --> 
+  
+  <!-- #contents --> 
+  <!-- InstanceBeginEditable name="contents" -->
+  <div id="contents" style="min-height: 440px;">
+    <div class="contents_wrapper">
+        <!-- Content -->
+        @yield('content')
+        <!-- /Content -->
+    </div>
+  </div>  
+  <!-- InstanceEndEditable --> 
+  <!-- /#contents --> 
+  <!-- #footer -->
+  <footer id="global-footer">
+    <div class="footer_contents">
+      <div class="footer_links">
+        <div id="fnav">
+          <ul>
+            <li><a href="http://www.cis.ac.jp/sitemap/index.html">サイトマップ</a></li>
+            <li><a href="http://www.cis.ac.jp/privacy/index.html">プライバシーポリシー</a></li>
+            <li><a href="http://www.cis.ac.jp/copyright/index.html">コピーライト表記</a></li>
+          </ul>
+        </div>
+        <div id="back_to_top">
+          <p><a href="http://www.cis.ac.jp/index.html#wrapper"><img src="{{ asset('') }}public/common/img/btn_bk_ptop_off.png" alt="このページの先頭へ" width="211" height="50"></a></p>
+        </div>
+        <div id="back_to_home">
+          <p><a href="http://www.cis.ac.jp/index.html"><img src="{{ asset('') }}public/common/img/btn_bk_home_off.png" alt="ホーム" width="227" height="32"></a></p>
+        </div>
+      </div>
+      <div class="copyright">
+        <div class="copyright_inner">
+          <address class="gf-address">
+            <h3>千葉科学大学</h3>
+            <p>〒288-0025　千葉県銚子市潮見町3番 0479-30-4500（代表）</p>
+          </address>
+          <small class="gf-copyright">Copyright ©2015 CHIBA INSTITUTE OF SCIENCE. All Right Reserved.</small>
+        </div>
+      </div>
+    </div>
+  </footer>
+  <!-- /#footer --> 
+</div>
+<div id="scroll_top" style="display: none;">
+  <a href="http://www.cis.ac.jp/index.html#wrapper"><img src="{{ asset('') }}public/common/img/top_arrow_off.png" width="38" height="180" alt="このページの先頭へ"></a>
+</div>
+
+<!-- InstanceEnd --></body></html>
---
+++ resources/views/frontend/search/index.blade.php
@@ -0,0 +1,17 @@
+@extends('frontend.layouts.app')
+
+@section('content')
+<div id="pnav">
+        <ul>
+          <li><a href="http://www.cis.ac.jp/index.html">千葉科学大学ホーム</a></li>          
+          <li>学部・学科・大学院・別科紹介</li>
+        </ul>
+      </div>
+      
+      <div id="main-full">
+        <header class="article-header">
+          <h1>学部・学科・大学院・別科紹介</h1>
+        </header> 
+              <p>医療に対する使命感と倫理観にあふれる、くすりの専門家の育成を目指します。</p>
+          </div> 
+@endsection
