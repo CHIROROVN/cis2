ed38028d6d27486043cb7ca6c7a048f2282a3573
hoaunet <hoaunet@yahoo.com>
2017-11-09 09:04:50 +0700

    u

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,7 +11,9 @@
 |
 */
 
-Route::get('/', 'WelcomeController@index');
+Route::get('/', function(){
+	return redirect()->route('frontend.search.index');
+});
 
 Route::get('home', 'HomeController@index');
 
