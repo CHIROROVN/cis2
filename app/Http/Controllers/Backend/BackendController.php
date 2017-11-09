893f7443eb5804e92fe1adfaae78023872395676
phuong.nd@chiroro.com.vn <phuong.nd@chiroro.com.vn>
2017-11-09 13:26:45 +0700

    update

--- app/Http/Controllers/Backend/BackendController.php
+++ app/Http/Controllers/Backend/BackendController.php
@@ -15,6 +15,24 @@ class BackendController extends Controller
             define($key, $value);
         }
 
+        //get IP address from user
+        if (isset($_SERVER['HTTP_CLIENT_IP']))
+            $ipaddress = $_SERVER['HTTP_CLIENT_IP'];
+        else if(isset($_SERVER['HTTP_X_FORWARDED_FOR']))
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        else if(isset($_SERVER['HTTP_X_FORWARDED']))
+            $ipaddress = $_SERVER['HTTP_X_FORWARDED'];
+        else if(isset($_SERVER['HTTP_FORWARDED_FOR']))
+            $ipaddress = $_SERVER['HTTP_FORWARDED_FOR'];
+        else if(isset($_SERVER['HTTP_FORWARDED']))
+            $ipaddress = $_SERVER['HTTP_FORWARDED'];
+        else if(isset($_SERVER['REMOTE_ADDR']))
+            $ipaddress = $_SERVER['REMOTE_ADDR'];
+        else
+            $ipaddress = '';
+
+        define('CLIENT_IP_ADRS', $ipaddress);
+
     }
 
 }
