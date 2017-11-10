78375daf4e7dc16b9eeea685baf050bf7b559322
hoaunet <hoaunet@yahoo.com>
2017-11-10 11:26:39 +0700

    Update common

--- resources/lang/ja/common.php
+++ resources/lang/ja/common.php
@@ -15,5 +15,11 @@ return [
     'msg_cts-adm_del_danger'              => '削除に失敗しました。',
     'msg_cts-adm_regist_danger'           => 'レジスタが失敗しました。',
     'msg_cts-adm_edit_danger'             => '更新に失敗しました。',
-
+    //
+    'msg_regist_success'                => '登録が完了しました。',
+    'msg_regist_danger'                 => '登録できませんでした。',
+    'msg_edit_success'                  => '編集箇所を保存しました。',
+    'msg_edit_danger'                   => '編集箇所を保存できませんでした。',
+    'msg_delete_success'                => '削除しました。',
+    'msg_delete_danger'                 => '削除できませんでした。',
 ];
