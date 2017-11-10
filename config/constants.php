546654dd16e401203fd567361111e9d909175c26
phuong.nd@chiroro.com.vn <phuong.nd@chiroro.com.vn>
2017-11-10 16:26:50 +0700

    update

--- config/constants.php
+++ config/constants.php
@@ -4,8 +4,6 @@
 	            'INSERT'                => 0,
 	            'UPDATE'                => 1,
 	            'DELETE'                => 9,
-	            'START_YEAR'            => 1937,
-	            'END_YEAR'            => date("Y"),
+	            'LIMIT_PAGE'            => 2,
 	        )
-
         ];
