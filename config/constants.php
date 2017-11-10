a599fed787cafc1da70c6bd50197dbb41ff8579d
hoaunet <hoaunet@yahoo.com>
2017-11-10 16:57:47 +0700

    upsa

---
+++ config/constants.php
@@ -0,0 +1,16 @@
+<?php
+    return [
+	        'DEFINE' => array(
+	            'INSERT'                => 0,
+	            'UPDATE'                => 1,
+	            'DELETE'                => 9,
+<<<<<<< HEAD
+	            'START_YEAR'            => 1937,
+	            'END_YEAR'            => date("Y"),
+	            'LIMIT_PAGE'            => 2,
+
+=======
+	            'LIMIT_PAGE'            => 2,
+>>>>>>> 546654dd16e401203fd567361111e9d909175c26
+	        )
+        ];
