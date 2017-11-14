5b1e99202a7bdf0a4d3c1eb782317fe38087fb70
phuong.nd@chiroro.com.vn <phuong.nd@chiroro.com.vn>
2017-11-14 16:06:26 +0700

    update

--- resources/lang/ja/validation.php
+++ resources/lang/ja/validation.php
@@ -23,12 +23,15 @@ return [
 
     //Faculty
     'error_faculty_name_required'         => '学部/研究科の名称を入力してください。',
+    'error_faculty_name_regex'            => '学部/研究科の名称を入力してください。',
 
     //Dept
     'error_dept_name_required'            => '学科/専攻の名称を入力してください。',
+    'error_dept_name_regex'               => '学科/専攻の名称を入力してください。',
 
     //Research
     'error_research_name_required'        => '研究分野の名称を入力してください。',
+    'error_research_name_regex'           => '研究分野の名称を入力してください。',
 
     'error_teacher_name_regex'            => 'ひらがなで入力してください。',
     
